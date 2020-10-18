6727bdfc45ae5c8f3d8b26f91c8aa96256d395af
shazena <62306982+shazena@users.noreply.github.com>
2020-10-18 04:00:28 -0400

    Add Empty Project File with Dependencies

---
+++ BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/BlogFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skshazena.blogFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogFinalProjectApplication.class, args);
+	}
+
+}
