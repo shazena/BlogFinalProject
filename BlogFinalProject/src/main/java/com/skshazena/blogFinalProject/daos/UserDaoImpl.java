83a22529869f20e38dc39bc32fbf1deaa51edad4
shazena <62306982+shazena@users.noreply.github.com>
2020-10-18 19:07:04 -0400

    Implement UserDaoDB

--- BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/UserDaoImpl.java
+++ BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/UserDaoImpl.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -27,33 +28,77 @@ public class UserDaoImpl implements UserDao {
     JdbcTemplate jdbc;
 
     @Override
-    public User getUserById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public User getUserById(int userId) {
+        try {
+            final String SELECT_USER_BY_ID = "SELECT * FROM user WHERE userId = ?";
+            User user = jdbc.queryForObject(SELECT_USER_BY_ID, new UserMapper(), userId);
+            user.setRoles(getRolesForUser(user.getUserId()));
+            return user;
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public User getUserByUsername(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            final String SELECT_USER_BY_USERNAME = "SELECT * FROM user WHERE username = ?";
+            User user = jdbc.queryForObject(SELECT_USER_BY_USERNAME, new UserMapper(), username);
+            user.setRoles(getRolesForUser(user.getUserId()));
+            return user;
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String SELECT_ALL_USERS = "SELECT * FROM user";
+        List<User> users = jdbc.query(SELECT_ALL_USERS, new UserMapper());
+        for (User user : users) {
+            user.setRoles(getRolesForUser(user.getUserId()));
+        }
+        return users;
     }
 
     @Override
     public void updateUser(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String UPDATE_USER = "UPDATE user SET username = ?, password = ?,enabled = ? WHERE userId = ?";
+        jdbc.update(UPDATE_USER,
+                user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.getUserId());
+
+        final String DELETE_USER_ROLE = "DELETE FROM userRole WHERE user_id = ?";
+        jdbc.update(DELETE_USER_ROLE, user.getUserId());
+        for (Role role : user.getRoles()) {
+            final String INSERT_USER_ROLE = "INSERT INTO userRole(user_id, role_id) VALUES(?,?)";
+            jdbc.update(INSERT_USER_ROLE, user.getUserId(), role.getRoleId());
+        }
     }
 
     @Override
-    public void deleteUser(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleteUser(int userId) {
+        final String DELETE_USER_ROLE = "DELETE FROM userRole WHERE user_id = ?";
+        final String DELETE_USER = "DELETE FROM user WHERE userId = ?";
+        jdbc.update(DELETE_USER_ROLE, userId);
+        jdbc.update(DELETE_USER, userId);
     }
 
     @Override
+    @Transactional
     public User createUser(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String INSERT_USER = "INSERT INTO user(username, password, enabled) VALUES(?,?,?)";
+        jdbc.update(INSERT_USER, user.getUsername(), user.getPassword(), user.isEnabled());
+        int newId = jdbc.queryForObject("select LAST_INSERT_ID()", Integer.class);
+        user.setUserId(newId);
+
+        for (Role role : user.getRoles()) {
+            final String INSERT_USER_ROLE = "INSERT INTO user_role(user_id, role_id) VALUES(?,?)";
+            jdbc.update(INSERT_USER_ROLE, user.getUserId(), role.getRoleId());
+        }
+        return user;
     }
 
     //Helper Methods
