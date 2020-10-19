8865c87454e585da9e6cfc70a773f71477ac7e4a
shazena <62306982+shazena@users.noreply.github.com>
2020-10-18 20:09:07 -0400

    Complete CommentDao and CommentDaoImpl

--- BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/CommentDao.java
+++ BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/CommentDao.java
@@ -1,5 +1,8 @@
 package com.skshazena.blogFinalProject.daos;
 
+import com.skshazena.blogFinalProject.dtos.Comment;
+import java.util.List;
+
 /**
  *
  * @author Shazena Khan
@@ -7,4 +10,20 @@ package com.skshazena.blogFinalProject.daos;
  * Date Created: Oct 18, 2020
  */
 public interface CommentDao {
+
+    Comment getCommentById(int commentId);
+
+    List<Comment> getAllComments();
+
+    List<Comment> getAllCommentsForPost(int postId);
+
+    List<Comment> getAllCommentsWrittenByUser(int userId);
+
+    List<Comment> getAllCommentsNeedingApproval();
+
+    void updateComment(Comment comment);
+
+    void deleteComment(int commentId);
+
+    Comment createComment(Comment comment);
 }
--- BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/CommentDaoImpl.java
+++ BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/daos/CommentDaoImpl.java
@@ -4,13 +4,17 @@ import com.skshazena.blogFinalProject.daos.PostDaoImpl.PostMapper;
 import com.skshazena.blogFinalProject.daos.PostDaoImpl.UserMapper;
 import com.skshazena.blogFinalProject.dtos.Comment;
 import com.skshazena.blogFinalProject.dtos.Post;
+import com.skshazena.blogFinalProject.dtos.Role;
 import com.skshazena.blogFinalProject.dtos.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -24,12 +28,129 @@ public class CommentDaoImpl implements CommentDao {
     @Autowired
     JdbcTemplate jdbc;
 
-//    private String title;
-//    private String content;
-//    private LocalDateTime createdAt;
-//    private boolean approvalStatus;
-//    private Post post;
-//    private User user;
+    @Override
+    public Comment getCommentById(int commentId) {
+        try {
+            final String SELECT_COMMENT_BY_ID = "SELECT * FROM Comment "
+                    + "WHERE commentId = ?";
+            Comment comment = jdbc.queryForObject(SELECT_COMMENT_BY_ID, new CommentMapper(), commentId);
+            associateUserAndPostWithComment(comment);
+            return comment;
+        } catch (DataAccessException e) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public List<Comment> getAllComments() {
+        final String SELECT_COMMENTS = "SELECT * FROM Comment";
+
+        List<Comment> allComments = jdbc.query(SELECT_COMMENTS, new CommentMapper());
+
+        for (Comment comment : allComments) {
+            associateUserAndPostWithComment(comment);
+        }
+
+        return allComments;
+
+    }
+
+    @Override
+    public List<Comment> getAllCommentsForPost(int postId) {
+
+        final String SELECT_COMMENTS_FOR_POST = "SELECT * from Comment "
+                + "WHERE postId = ?";
+
+        List<Comment> commentsForPost = jdbc.query(SELECT_COMMENTS_FOR_POST, new CommentMapper(), postId);
+
+        for (Comment comment : commentsForPost) {
+            associateUserAndPostWithComment(comment);
+        }
+        return commentsForPost;
+    }
+
+    @Override
+    public List<Comment> getAllCommentsWrittenByUser(int userId) {
+        final String SELECT_COMMENTS_FOR_USER = "SELECT * from Comment "
+                + "WHERE userId = ?";
+
+        List<Comment> commentsForUser = jdbc.query(SELECT_COMMENTS_FOR_USER, new CommentMapper(), userId);
+
+        for (Comment comment : commentsForUser) {
+            associateUserAndPostWithComment(comment);
+        }
+
+        return commentsForUser;
+    }
+
+    @Override
+    public List<Comment> getAllCommentsNeedingApproval() {
+        final String SELECT_COMMENTS_NEEDING_APPROVAL = "SELECT * from Comment "
+                + "WHERE approvalStatus = 0";
+
+        List<Comment> commentsNeedingApproval = jdbc.query(SELECT_COMMENTS_NEEDING_APPROVAL, new CommentMapper());
+
+        for (Comment comment : commentsNeedingApproval) {
+            associateUserAndPostWithComment(comment);
+        }
+
+        return commentsNeedingApproval;
+    }
+
+    @Override
+    @Transactional
+    public void updateComment(Comment comment) {
+        final String UPDATE_COMMENT = "UPDATE comment SET "
+                + "title = ?, "
+                + "content = ?, "
+                + "createdAt = ?, "
+                + "approvalStatus = ?, "
+                + "postId = ?, "
+                + "userId = ? "
+                + "WHERE commentId = ?";
+
+        jdbc.update(UPDATE_COMMENT,
+                comment.getTitle(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.isApprovalStatus(),
+                comment.getPost().getPostId(),
+                comment.getUser().getUserId(),
+                comment.getCommentId());
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteComment(int commentId) {
+        final String DELETE_COMMENT = "DELETE FROM Comment "
+                + "WHERE commentId = ?";
+        jdbc.update(DELETE_COMMENT, commentId);
+    }
+
+    @Override
+    @Transactional
+    public Comment createComment(Comment comment) {
+        final String INSERT_COMMENT = "INSERT INTO Comment(title, content, createdAt, approvalStatus, postId, userId) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        jdbc.update(INSERT_COMMENT,
+                comment.getTitle(),
+                comment.getContent(),
+                comment.getCreatedAt().withNano(0),
+                comment.isApprovalStatus(),
+                comment.getPost().getPostId(),
+                comment.getUser().getUserId());
+
+        int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        comment.setCommentId(newId);
+
+        associateUserAndPostWithComment(comment);
+
+        return comment;
+
+    }
+
     //Helper methods
     private User getUserForComment(int commentId) {
         final String SELECT_USER_FOR_COMMENT = "SELCT u.* from User u "
