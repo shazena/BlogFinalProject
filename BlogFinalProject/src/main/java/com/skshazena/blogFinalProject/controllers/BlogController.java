7ffa91d182baf7e37b831666f0f3027fde480301
shazena <62306982+shazena@users.noreply.github.com>
2020-10-21 19:00:14 -0400

    Add Older comments to the blog post page

--- BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/controllers/BlogController.java
+++ BlogFinalProject/src/main/java/com/skshazena/blogFinalProject/controllers/BlogController.java
@@ -1,5 +1,6 @@
 package com.skshazena.blogFinalProject.controllers;
 
+import com.skshazena.blogFinalProject.dtos.Comment;
 import com.skshazena.blogFinalProject.dtos.Post;
 import com.skshazena.blogFinalProject.service.BlogFinalProjectService;
 import java.util.List;
@@ -26,12 +27,14 @@ public class BlogController {
     public String getPostDetailsOnPage(Integer id, Model model) {
         List<Post> staticPosts = service.getAllPostsForBlogThatAreStaticNewestFirst();
         Post post = service.getPostById(id);
+        List<Comment> allCommentsForPost = service.getAllCommentsForPost(id);
 
         List<Post> olderAndNewerPosts = service.getOlderAndNewerPost(id);
         Post olderPost = olderAndNewerPosts.get(0);
         Post newerPost = olderAndNewerPosts.get(1);
 
         model.addAttribute("staticPosts", staticPosts);
+        model.addAttribute("comments", allCommentsForPost);
         model.addAttribute("post", post);
         model.addAttribute("olderPost", olderPost);
         model.addAttribute("newerPost", newerPost);
