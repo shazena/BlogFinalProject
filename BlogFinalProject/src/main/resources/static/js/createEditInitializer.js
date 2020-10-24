eda3c7361e05944630af27de1e18f34cb1c65597
shazena <62306982+shazena@users.noreply.github.com>
2020-10-23 22:23:45 -0400

    Remove Some TinyMCE features not supported by the blog

--- BlogFinalProject/src/main/resources/static/js/createEditInitializer.js
+++ BlogFinalProject/src/main/resources/static/js/createEditInitializer.js
@@ -14,10 +14,11 @@ var header = $("meta[name='_csrf_header']").attr("content");
 
 tinymce.init({
     selector: '#content',
-    plugins: 'print preview paste searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+    plugins: 'print preview paste searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons',
     menubar: 'file edit view insert format tools table help',
-    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | charmap emoticons | fullscreen  preview print | insertfile image media link anchor codesample',
     toolbar_sticky: true,
+    content_css : "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
     image_advtab: true,
     /* enable title field in the Image dialog*/
     image_title: true,
